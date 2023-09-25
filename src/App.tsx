c14794780a9c4854eb52eefe2d3a091685a802b2
tsteelematc <tsteele@madisoncollege.edu>
2023-09-25 13:29:58 -0500

    Installed MUI and made sure it's working...
    npm install @mui/material @emotion/react @emotion/styled
    
    npm install @fontsource/roboto
    
    npm install @mui/icons-material

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import SmartDisplay from '@mui/icons-material/SmartDisplay';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        variant="outlined"
+        size="large"
+        startIcon={
+          <SmartDisplay />
+        }
+      >
+        Play a Game of Foo
+      </Button>
     </div>
   );
 }
