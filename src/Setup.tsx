98e96e4428c5e0535db8eec261a7d8fd357fbc6b
tsteelematc <tsteele@madisoncollege.edu>
2023-10-02 10:32:41 -0500

    This seems a good way to review useState()

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,20 +1,30 @@
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 export const Setup = () => {
 
     const navigate = useNavigate();
 
+    // let num = 1;
+
+    const [num, setNum] = useState(1);
+
     return (
         <>
             <h3>
-                Setup a Game of Foo
+                Setup a Game of Foo ({num})
             </h3>
             <Button
                 variant="outlined"
                 size="large"
                 onClick={
-                    () => navigate('/play')
+                    // () => navigate('/play')
+                    () => {
+                        //num++;
+                        setNum(num + 1);
+                        console.log(num);
+                    }
                 }
             >
                 Start the Game
