4b9006eb4e69adfc573e58b0c72897590be0d575
mservillon <mackservillon@gmail.com>
2023-10-09 13:40:06 -0500

    Initial styling done
    Next we'll move on to more fun facts and data experiments

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,9 @@ import SmartDisplay from '@mui/icons-material/SmartDisplay';
 import { useNavigate } from "react-router-dom";
 import { WinningPercentageDisplay } from './foo-game-results';
 import { FC } from 'react';
+import { Typography, Paper, Table, TableBody, TableRow, TableCell } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+
 
 interface HomeProps {
     winningPercentageDisplay: WinningPercentageDisplay;
@@ -17,17 +20,88 @@ export const Home: FC<HomeProps> = ({winningPercentageDisplay}) => {
             Foo Companion App
         </h3>
         <Button
-            variant="outlined"
+            variant="contained"
             size="large"
-            startIcon={
-            <SmartDisplay />
-            }
+            sx={{
+                mt: 3
+                , pt: 3
+                , pb: 3
+                , width: {
+                    xs: '100%'
+                    , md: 'inherit'
+                }
+            }}
             onClick={
                 () => navigate('/setup')
             }
         >
-            Play a Game of Foo
+            <Typography
+                fontSize={20}
+            >
+                Play a Game of Foo
+            </Typography>
         </Button>
+        <Grid
+            container
+            spacing={3}
+        >
+            <Grid
+                xs={12}
+                md={6}
+            >
+                <Paper
+                    elevation={3}
+                    sx={{
+                        overflow: 'hidden'
+                    }}
+                >
+                    <Typography
+                        sx={{
+                            fontSize: 20
+                            , ml: 3
+                            , mt: 3
+                            , mb: 3
+                        }}
+                    >
+                    </Typography>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <Typography
+                                        fontSize={20}
+                                    >
+                                        Total Games
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography
+                                        fontSize={20}
+                                    >
+                                    {winningPercentageDisplay.totalGames}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography
+                                        fontSize={20}
+                                    >
+                                        Winning Percentage
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography
+                                        fontSize={20}
+                                    >
+                                        {winningPercentageDisplay.winningPercentage}
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+
+                </Paper>
+            </Grid>
+        </Grid>
         <h4>
             {`Winning Percentage: ${winningPercentageDisplay.winningPercentage}`}
         </h4>
