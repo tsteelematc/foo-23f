9fde6a6434f01bbc976051116e05f3e5c2858f78
tsteelematc <tsteele@madisoncollege.edu>
2023-10-18 07:46:48 -0500

    This is better for first demo...
    And interesting that don't 'need' local state, but it is a better choice in the long run once the Play component does more ! ! !
    
    Maybe too much nuance to actually discuss, but fun ! ! !

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,7 +1,7 @@
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
 import { GameResult } from './foo-game-results';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 interface PlayProps {
     addNewGameResult: (r: GameResult) => void;
@@ -11,11 +11,26 @@ export const Play: FC<PlayProps> = ({addNewGameResult}) => {
 
     const nav = useNavigate();
 
+    //
+    // As local state...
+    // . initialized once
+    // . never call setStart so use underscore convention, i-o-g
+    //
+    const [start, _] = useState(new Date().toISOString());
+    
+    //
+    // Demo that simple const doesn't work, cause our FC 
+    // gets called more than once so games will always be 0s.
+    //
+    // const start = new Date().toISOString();
+    // console.log('initialized start const', start);
+
     const gameOver = (won: boolean) => {
+        // console.log('gameOver()', start);
         addNewGameResult({
             won: won
-            , start: ""
-            , end: ""
+            , start: start
+            , end: new Date().toISOString()
         });
         nav(-2);
     };
