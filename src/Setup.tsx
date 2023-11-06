cfab60adb1c98803f4badec4878edc346b1f2ba3
tsteelematc <tsteele@madisoncollege.edu>
2023-11-06 13:31:26 -0600

    Can now get the players that have been chosen...
    Maybe next, let's make sure at least one player is chosen before they can start the game...

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -18,6 +18,12 @@ export const Setup: FC<SetupProps> = ({
     , previousPlayers
 }) => {
 
+    const [availablePlayers, setAvailablePlayers] = 
+        useState(previousPlayers.map(x => ({
+            name: x 
+            , checked: false
+        })));
+
     useEffect(
         () => setTitle("Game Setup")
         , []
@@ -60,7 +66,7 @@ export const Setup: FC<SetupProps> = ({
                 }}
             >
                 {
-                    previousPlayers.map(x => (
+                    availablePlayers.map(x => (
                         <Grid
                             xs={12}
                             sm={6}
@@ -69,10 +75,21 @@ export const Setup: FC<SetupProps> = ({
                         >
                             <FormControlLabel 
                                 control={
-                                    <Checkbox 
+                                    <Checkbox
+                                        checked={x.checked} 
+                                        onChange={
+                                            (e) => setAvailablePlayers(
+                                                [
+                                                    ...availablePlayers.map(y => ({
+                                                        name: y.name
+                                                        , checked: y.name == x.name ? !y.checked : y.checked
+                                                    }))
+                                                ]
+                                            )
+                                        }
                                     />
                                 } 
-                                label={x} 
+                                label={x.name} 
                             />
                         </Grid>
                     ))
