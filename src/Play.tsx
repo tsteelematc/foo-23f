e65f3a2acff916e11a35338b6837831edc7101c2
tsteelematc <tsteele@madisoncollege.edu>
2023-11-13 13:55:53 -0600

    Made our new fancy fun fact together...
    Co-Authored-By: Jonah <91928902+jonahhhhhh@users.noreply.github.com>
    Co-Authored-By: mservillon <119347806+mservillon@users.noreply.github.com>
    Co-Authored-By: Sheldon <91235824+KasebrotII@users.noreply.github.com>

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,6 +1,6 @@
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
-import { GameResult } from './foo-game-results';
+import { GameResult, Player } from './foo-game-results';
 import { FC, useEffect, useState } from 'react';
 import { Box, ButtonGroup, FormControlLabel, Switch, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { Add, Remove, ThumbDown, ThumbDownSharp, ThumbUpSharp, ThumbsUpDown } from '@mui/icons-material';
@@ -17,6 +17,20 @@ export const Play: FC<PlayProps> = ({
     , chosenPlayers
 }) => {
 
+    const [startTimestamp, _] = useState(new Date().toISOString());
+    // _("")
+
+    const [turnNumber, setTurnNumber] = useState<number>(1)
+
+    const [inGamePlayers, setInGamePlayers] = useState<Player[]>(
+        chosenPlayers.map(
+            x => ({
+                name: x
+                , turns: []
+            })
+        )
+    );
+
     useEffect(
         () => setTitle(`Track turn "feelings"...`)
         , []
@@ -24,9 +38,6 @@ export const Play: FC<PlayProps> = ({
 
     const nav = useNavigate();
 
-    const [startTimestamp, _] = useState(new Date().toISOString());
-
-    // _("")
 
     // Could only destructure first item, but I like underscore convention to 
     // mean 'unused'.
@@ -36,10 +47,7 @@ export const Play: FC<PlayProps> = ({
         addNewGameResult({
 
             winner: winner
-            , players: chosenPlayers.map(x => ({
-                name: x
-                , turns: []
-            }))
+            , players: inGamePlayers
             
             , start: startTimestamp
             , end: new Date().toISOString()
@@ -66,16 +74,29 @@ export const Play: FC<PlayProps> = ({
             >
                 <Button
                     variant='contained'
+                    onClick={
+
+                        // () => setTurnNumber(
+                        //     turnNumber > 1
+                        //     ? turnNumber - 1
+                        //     : turnNumber
+                        // )
+
+                        () => turnNumber > 1 && setTurnNumber(turnNumber - 1)
+                    }
                 >
                     <Remove />
                 </Button>
                 <Typography
                     fontSize={20}
                 >
-                    Turn 1
+                    Turn {turnNumber}
                 </Typography>
                 <Button
                     variant='contained'
+                    onClick={
+                        () => setTurnNumber(turnNumber + 1)
+                    }
                 >
                     <Add />
                 </Button>
@@ -92,10 +113,41 @@ export const Play: FC<PlayProps> = ({
                             , gap: 2 
                         }}
                     >
-                        {`How was ${x}'s Turn 1?`}
+                        {`How was ${x}'s Turn ${turnNumber}?`}
                         <ToggleButtonGroup
                             exclusive
-                            value="Thumbs Up"
+                            value={
+                                inGamePlayers
+                                    .find(
+                                        y => y.name == x
+                                    )
+                                    ?.turns.find(
+                                        y => y.num == turnNumber
+                                    )
+                                    ?.status
+                            }
+
+                            onChange={
+                                // Holy Hell
+                                (e, value) => setInGamePlayers(
+                                    inGamePlayers.map(
+                                        y => ({
+                                            name: y.name
+                                            , turns: y.name == x
+                                                ? [
+                                                    ...y.turns.filter(
+                                                        z => z.num != turnNumber
+                                                    )
+                                                    , {
+                                                        num: turnNumber
+                                                        , status: value
+                                                    }
+                                                ]
+                                                : y.turns
+                                        })
+                                    )
+                                )
+                            }
                         >
                             <ToggleButton 
                                 value="Thumbs Down"
